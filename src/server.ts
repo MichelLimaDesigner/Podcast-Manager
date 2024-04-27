6e86125ed50e80e846e5ce011a33d1d960b1a82e
michellimadesigner <michellimadesigner@outlook.com>
2024-04-27 20:00:02 -0300

    feat: add server and dotenv lib

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("Hellow World");
+import * as http from "http";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const server = http.createServer(
+  (request: http.IncomingHttpHeaders, response: http.ServerResponse) => {}
+);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`);
+});

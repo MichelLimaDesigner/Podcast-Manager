4b9cbfbe08df0066999684d1e20ad01cb3ce08be
michellimadesigner <michellimadesigner@outlook.com>
2024-04-28 22:40:11 -0300

    refactor: server file

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import * as http from "http";
+import {
+  getFilterEpisodes,
+  getListEpisodes,
+} from "./controllers/podcasts-controller";
+import { HttpMethod } from "./utils/http-method";
+import { Routes } from "./routes/routes";
+
+export const app = async (
+  request: http.IncomingMessage,
+  response: http.ServerResponse
+) => {
+  // queryString
+  const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
+
+  // List podcasts
+  if (request.method === HttpMethod.GET && baseUrl === Routes.LIST)
+    await getListEpisodes(request, response);
+
+  // Filter podcasts episodes
+  if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE)
+    await getFilterEpisodes(request, response);
+};
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,10 @@
 import * as http from "http";
 import * as dotenv from "dotenv";
-import {
-  getFilterEpisodes,
-  getListEpisodes,
-} from "./controllers/podcasts-controller";
-import { Routes } from "./routes/routes";
-import { HttpMethod } from "./utils/http-method";
+import { app } from "./app";
 
 dotenv.config();
 
-const server = http.createServer(
-  async (request: http.IncomingMessage, response: http.ServerResponse) => {
-    // queryString
-    const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
-
-    // List podcasts
-    if (request.method === HttpMethod.GET && baseUrl === Routes.LIST)
-      await getListEpisodes(request, response);
-
-    // Filter podcasts episodes
-    if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE)
-      await getFilterEpisodes(request, response);
-  }
-);
+const server = http.createServer(app);
 
 const port = process.env.PORT;
 
